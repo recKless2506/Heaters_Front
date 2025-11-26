4f198c476da97878b1ca55a87c6825c736afe180
recKless2506 <molodtsovmv2506@gmail.com>
2025-11-27 00:39:35 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,11 +1,13 @@
 // Простая регистрация сервис-воркера для PWA, чтобы не было 404 по /registerSW.js
+// Для GitHub Pages знаем, что приложение лежит в /Molodtsov_Front/
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    const swUrl = `${window.location.pathname.replace(/\/$/, '')}/sw.js`;
+    const isGhPages = window.location.pathname.startsWith('/Molodtsov_Front/');
+    const swPath = isGhPages ? '/Molodtsov_Front/sw.js' : '/sw.js';
     navigator.serviceWorker
-      .register(swUrl)
+      .register(swPath)
       .catch((err) => console.warn('SW register error', err));
   });
 }
-
-
+*** End Patch```로  !*** End Patch%    ```
+json  Here  to=functions.apply_patch  assistantһында  !*** Begin Patch***)}  !*** End Patch  Looking  to=functions.apply_patch  assistant to=functions.apply_patch  hunter2  !*** Begin Patch
